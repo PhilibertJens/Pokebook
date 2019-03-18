29a52c5f52ee45e4f71ad7d4eb43a33cfa1a98e1
jens philibert <jens.philibert@hotmail.be>
2019-03-18 10:21:46 +0100

    Added properties to Message class

--- Pokebook.api/Models/Message.cs
+++ Pokebook.api/Models/Message.cs
@@ -7,5 +7,17 @@ namespace Pokebook.api.Models
 {
     public class Message
     {
+        public Guid Id { get; set; }
+        public Guid ChatId { get; set; }
+        public Guid SenderId { get; set; }
+
+        public string Text { get; set; }
+        public string ImageName { get; set; }
+
+        public DateTime SendDate { get; set; }
+
+        /*Navigation properties*/
+        public Chat Chat { get; set; }
+        public User Sender { get; set; }
     }
 }
