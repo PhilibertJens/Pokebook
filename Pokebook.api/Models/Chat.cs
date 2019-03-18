975ad0a3d097653a69237aeca6dcbfcee9eb05db
jens philibert <jens.philibert@hotmail.be>
2019-03-18 10:16:44 +0100

    Added properties to Chat class

--- Pokebook.api/Models/Chat.cs
+++ Pokebook.api/Models/Chat.cs
@@ -7,5 +7,20 @@ namespace Pokebook.api.Models
 {
     public class Chat
     {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public string Theme { get; set; }
+
+        public Guid CreatorId { get; set; }
+        public DateTime CreateDate { get; set; }
+
+        public int NumberOfMessages { get; set; }
+        public int NumberOfUsers { get; set; }
+        public string LastMessage { get; set; }
+
+        /*Navigation properties*/
+        public ICollection<UserChat> UserChats { get; set; }
+        public ICollection<Message> Messages { get; set; }
     }
 }
