b7ac6774fdb8d850097917a670bad493535bc169
jens philibert <jens.philibert@hotmail.be>
2019-03-19 10:09:13 +0100

    Added IRepository interface, Repository class and Entitybase abstract class

---
+++ Pokebook.api/Repositories/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using Pokebook.api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pokebook.api.Repositories
+{
+    public interface IRepository<T> where T : EntityBase
+    {
+    }
+}
---
+++ Pokebook.api/Repositories/Repository.cs
@@ -0,0 +1,12 @@
+﻿using Pokebook.api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pokebook.api.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : EntityBase
+    {
+    }
+}
