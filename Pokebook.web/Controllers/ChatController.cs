e690a8c042ac9d4ebbad449f0a0107781431d6e6
jens philibert <jens.philibert@hotmail.be>
2019-03-23 11:10:06 +0100

    Converted jsonString to ListChat on ChatController and added ChatIndexVM

--- Pokebook.web/Controllers/ChatController.cs
+++ Pokebook.web/Controllers/ChatController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Pokebook.api.Models;
 
 namespace Pokebook.web.Controllers
 {
@@ -14,6 +16,8 @@ namespace Pokebook.web.Controllers
                         "{'name': 'Awesome Chat','image': null,'theme': null,'creatorId': '00000000-0000-0000-0000-000000000000','createDate': '2019-03-19T11:51:57.857','numberOfMessages': 0,'numberOfUsers': 0,'lastMessage': null,'userChats': null,'messages': null,'id': '00000000-0000-0000-0000-000000000001'}," +
                         "{'name': 'Another awesome Chat','image': null,'theme': null,'creatorId': '00000000-0000-0000-0000-000000000000','createDate': '2019-03-19T09:51:57.859','numberOfMessages': 0,'numberOfUsers': 0,'lastMessage': null,'userChats': null,'messages': null,'id': '00000000-0000-0000-0000-000000000002'}" +
                         "]";
+
+            List<Chat> chatListForUser = JsonConvert.DeserializeObject <List<Chat>>(jsonString);
 
             return View();
         }
