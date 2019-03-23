37c629435e79b48785c5f398ec96367348ace4bb
jens philibert <jens.philibert@hotmail.be>
2019-03-23 11:43:37 +0100

    Added ViewComponent to show current Chats for user

--- Pokebook.web/Components/ChatListViewComponent.cs
+++ Pokebook.web/Components/ChatListViewComponent.cs
@@ -1,32 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
+﻿using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using Pokebook.api.Models;
 using Pokebook.web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
-namespace Pokebook.web.Controllers
+namespace Pokebook.web.Components
 {
-    public class ChatController : Controller
+    public class ChatListViewComponent : ViewComponent
     {
-        public IActionResult Index()
+        public async Task<IViewComponentResult> InvokeAsync()
         {
             var jsonString = "[" +
                         "{'name': 'Awesome Chat','image': null,'theme': null,'creatorId': '00000000-0000-0000-0000-000000000000','createDate': '2019-03-19T11:51:57.857','numberOfMessages': 0,'numberOfUsers': 0,'lastMessage': null,'userChats': null,'messages': null,'id': '00000000-0000-0000-0000-000000000001'}," +
                         "{'name': 'Another awesome Chat','image': null,'theme': null,'creatorId': '00000000-0000-0000-0000-000000000000','createDate': '2019-03-19T09:51:57.859','numberOfMessages': 0,'numberOfUsers': 0,'lastMessage': null,'userChats': null,'messages': null,'id': '00000000-0000-0000-0000-000000000002'}" +
                         "]";
 
-            List<Chat> chatListForUser = JsonConvert.DeserializeObject <List<Chat>>(jsonString);
-            ChatIndexVM vm = new ChatIndexVM
-            {
-                AllUserChatsForUser = chatListForUser,
-                User = new User { UserName = "UserWithNoName" },
-                AllUsers = new SelectList(new List<User>(), "Id", "UserName")
-            };
+            List<Chat> chatListForUser = JsonConvert.DeserializeObject<List<Chat>>(jsonString);
 
+            AllChatsForUserVM vm = new AllChatsForUserVM
+            {
+                ListChats = chatListForUser,
+                Username = "Jens"
+            };
             return View(vm);
         }
     }
