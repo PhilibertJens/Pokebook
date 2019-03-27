704fe10f33219aff0c92445b26cb3d219bbc45c5
jens philibert <jens.philibert@hotmail.be>
2019-03-27 14:19:01 +0100

    Added MessagesController and MessageRepository

---
+++ Pokebook.api/Controllers/MessagesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Pokebook.api.Models;
+using Pokebook.api.Repositories;
+
+namespace Pokebook.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessagesController : ControllerCrudBase<Message, MessageRepository>
+    {
+        public MessagesController(MessageRepository messageRepository):
+            base(messageRepository)
+        {
+        }
+    }
+}
---
+++ Pokebook.api/Repositories/MessageRepository.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Pokebook.api.Data;
+using Pokebook.api.Models;
+using Pokebook.api.Repositories.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pokebook.api.Repositories
+{
+    public class MessageRepository : MappingRepository<Message>
+    {
+        public MessageRepository(PokebookContext context, IMapper mapper):
+            base(context, mapper)
+        {
+        }
+    }
+}
