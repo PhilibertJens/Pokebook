301a941d3ea97c31fc971b532be201a78f67b2bf
Jonathan Boydens <jonathan_boydens@hotmail.com>
2019-03-28 08:11:34 +0100

    add base entity class + IRepo config
    - add: EntityBase.cs
      > contains Id of type Guid
    - mod: IRepository.cs
      > added definitions

--- Pokebook.core/Repositories/IRepository.cs
+++ Pokebook.core/Repositories/IRepository.cs
@@ -1,13 +1,24 @@
-﻿using System;
+﻿using Pokebook.core.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Pokebook.core.Repositories
 {
-    public interface IRepository : IBaseRepository //already inherits 'Count'
+    public interface IRepository<T> : IBaseRepository //already inherits 'Count'
+        where T : EntityBase
     {
-
+        Task<T> GetById(Guid id);
+        IQueryable<T> GetAll();
+        Task<IEnumerable<T>> ListAll();
+        IQueryable<T> GetFiltered(Expression<Func<T, bool>> predicate);
+        Task<IEnumerable<T>> ListFiltered(Expression<Func<T, bool>> predicate);
+        Task<T> Add(T entity);
+        Task<T> Delete(T entity);
+        Task<T> Delete(Guid id);
+        Task<T> Update(T entity);
     }
 }
