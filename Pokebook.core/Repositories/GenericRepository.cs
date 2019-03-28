fe90e4e942f465df2f9497bf7e86cc6e9c6561be
Jonathan Boydens <jonathan_boydens@hotmail.com>
2019-03-28 09:14:42 +0100

    add comment to generic repo methods
    - mod: GenericRepository.cs
      > added comments to methods, please read

--- Pokebook.core/Repositories/GenericRepository.cs
+++ Pokebook.core/Repositories/GenericRepository.cs
@@ -12,16 +12,23 @@ namespace Pokebook.core.Repositories
 {
     public abstract class GenericRepository<T> : IRepository<T> where T : EntityBase
     {
-        protected readonly PokebookContext db;
+        protected readonly PokebookContext db; //Look into implementation for Identity DbContext
         public GenericRepository(PokebookContext context)
         {
             db = context;
         }
         public int Count { get; set; }
-
+        /// <summary>
+        /// Finds all entries in repo, ready for deferred execution
+        /// </summary>
         public IQueryable<T> FindAll() => db.Set<T>().AsNoTracking();
         public async Task<T> FindByIdAsync(Guid id) => await db.Set<T>().FindAsync(id);
         public async Task<IEnumerable<T>> ListAll() => await FindAll().ToListAsync();
+
+        /// <summary>
+        /// Only to use for devs, with deferred execution
+        /// </summary>
+        /// <returns>Datasource which can be queried</returns>
         public IQueryable<T> FindFiltered(Expression<Func<T, bool>> predicate)
         {
             return db.Set<T>()
