aca907af01ad08dd7a11caf7440602d5ef4abbed
Jonathan Boydens <jonathan_boydens@hotmail.com>
2019-03-31 14:56:21 +0200

    merge w/master + resolving

--- Pokebook.api/Startup.cs
+++ Pokebook.api/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Pokebook.core.Data;
 using Pokebook.core.Repositories.Specific;
+using Pokebook.core.Services.Automapper;
 
 namespace Pokebook.api
 {
@@ -21,10 +22,19 @@ namespace Pokebook.api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var config = new AutoMapper.MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new AutoMapperProfileConfiguration());
+            }
+            );
+            var mapper = config.CreateMapper();
+            services.AddSingleton(mapper);
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             services.AddDbContext<PokebookContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("PokebookDb")));
             services.AddScoped<ChatRepository>();
+            services.AddScoped<MessageRepository>();
+            services.AddScoped<UserChatRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
