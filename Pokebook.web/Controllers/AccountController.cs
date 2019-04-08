3368b0bba91b4ab86f9ec3b880eb6b64c7c5a358
jens philibert <jens.philibert@hotmail.be>
2019-04-08 11:21:32 +0200

    Added Registration method to AccountController

--- Pokebook.web/Controllers/AccountController.cs
+++ Pokebook.web/Controllers/AccountController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Pokebook.web.Models;
 
 namespace Pokebook.web.Controllers
 {
@@ -12,5 +13,15 @@ namespace Pokebook.web.Controllers
         {
             return View();
         }
+
+        //login postmethod here
+
+        public IActionResult Registration()
+        {
+            AccountRegisterVM vm = new AccountRegisterVM();
+            return View(vm);
+        }
+
+        //register postmethod here
     }
 }
