a93eab2726f9b4b43d2191896f4e5da0c1e84788
jens philibert <jens.philibert@hotmail.be>
2019-04-08 17:20:24 +0200

    Added Migration with new FriendConnectionsTable

---
+++ Pokebook.core/Migrations/20190408151822_AddedFriendConnectionsTable.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Pokebook.core.Migrations
+{
+    public partial class AddedFriendConnectionsTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Messages",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000001"));
+
+            migrationBuilder.DeleteData(
+                table: "Messages",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000002"));
+
+            migrationBuilder.DeleteData(
+                table: "Messages",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000003"));
+
+            migrationBuilder.DeleteData(
+                table: "Chats",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000001"));
+
+            migrationBuilder.DeleteData(
+                table: "Chats",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000002"));
+
+            migrationBuilder.AddColumn<int>(
+                name: "NumberOfFriends",
+                table: "Users",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "FriendConnections",
+                columns: table => new
+                {
+                    IdRequester = table.Column<Guid>(nullable: false),
+                    IdApprover = table.Column<Guid>(nullable: false),
+                    Id = table.Column<Guid>(nullable: false),
+                    Created = table.Column<DateTime>(nullable: true, defaultValueSql: "GETDATE()"),
+                    Accepted = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FriendConnections", x => new { x.IdRequester, x.IdApprover });
+                    table.ForeignKey(
+                        name: "FK_FriendConnections_Users_Id",
+                        column: x => x.Id,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Chats",
+                columns: new[] { "Id", "CreateDate", "CreatorId", "Image", "LastMessage", "Name", "NumberOfMessages", "NumberOfUsers", "Theme" },
+                values: new object[] { new Guid("00000000-0000-0000-0000-000000000001"), new DateTime(2019, 4, 8, 17, 18, 21, 543, DateTimeKind.Local).AddTicks(5472), new Guid("00000000-0000-0000-0000-000000000000"), null, null, "Awesome Chat", 0, 0, null });
+
+            migrationBuilder.InsertData(
+                table: "Chats",
+                columns: new[] { "Id", "CreateDate", "CreatorId", "Image", "LastMessage", "Name", "NumberOfMessages", "NumberOfUsers", "Theme" },
+                values: new object[] { new Guid("00000000-0000-0000-0000-000000000002"), new DateTime(2019, 4, 8, 15, 18, 21, 545, DateTimeKind.Local).AddTicks(2409), new Guid("00000000-0000-0000-0000-000000000000"), null, null, "Another awesome Chat", 0, 0, null });
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000001"),
+                column: "Created",
+                value: new DateTime(2019, 4, 8, 17, 18, 22, 29, DateTimeKind.Local).AddTicks(4038));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000002"),
+                column: "Created",
+                value: new DateTime(2019, 4, 8, 17, 18, 22, 29, DateTimeKind.Local).AddTicks(4202));
+
+            migrationBuilder.InsertData(
+                table: "Messages",
+                columns: new[] { "Id", "ChatId", "ImageName", "SendDate", "SenderId", "Text" },
+                values: new object[] { new Guid("00000000-0000-0000-0000-000000000003"), new Guid("00000000-0000-0000-0000-000000000001"), null, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new Guid("00000000-0000-0000-0000-000000000001"), "This is a chat with myself. Is this even possible?" });
+
+            migrationBuilder.InsertData(
+                table: "Messages",
+                columns: new[] { "Id", "ChatId", "ImageName", "SendDate", "SenderId", "Text" },
+                values: new object[] { new Guid("00000000-0000-0000-0000-000000000001"), new Guid("00000000-0000-0000-0000-000000000002"), null, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new Guid("00000000-0000-0000-0000-000000000001"), "Hello Jon. This is Tyrion" });
+
+            migrationBuilder.InsertData(
+                table: "Messages",
+                columns: new[] { "Id", "ChatId", "ImageName", "SendDate", "SenderId", "Text" },
+                values: new object[] { new Guid("00000000-0000-0000-0000-000000000002"), new Guid("00000000-0000-0000-0000-000000000002"), null, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new Guid("00000000-0000-0000-0000-000000000002"), "Hello Tyrion. I'm Jon" });
+
+            migrationBuilder.UpdateData(
+                table: "UserChats",
+                keyColumns: new[] { "ChatId", "UserId" },
+                keyValues: new object[] { new Guid("00000000-0000-0000-0000-000000000001"), new Guid("00000000-0000-0000-0000-000000000001") },
+                column: "Created",
+                value: new DateTime(2019, 4, 8, 17, 18, 22, 46, DateTimeKind.Local).AddTicks(3211));
+
+            migrationBuilder.UpdateData(
+                table: "UserChats",
+                keyColumns: new[] { "ChatId", "UserId" },
+                keyValues: new object[] { new Guid("00000000-0000-0000-0000-000000000002"), new Guid("00000000-0000-0000-0000-000000000001") },
+                column: "Created",
+                value: new DateTime(2019, 4, 8, 17, 18, 22, 46, DateTimeKind.Local).AddTicks(3380));
+
+            migrationBuilder.UpdateData(
+                table: "UserChats",
+                keyColumns: new[] { "ChatId", "UserId" },
+                keyValues: new object[] { new Guid("00000000-0000-0000-0000-000000000002"), new Guid("00000000-0000-0000-0000-000000000002") },
+                column: "Created",
+                value: new DateTime(2019, 4, 8, 17, 18, 22, 46, DateTimeKind.Local).AddTicks(3290));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FriendConnections_Id",
+                table: "FriendConnections",
+                column: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FriendConnections");
+
+            migrationBuilder.DeleteData(
+                table: "Messages",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000001"));
+
+            migrationBuilder.DeleteData(
+                table: "Messages",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000002"));
+
+            migrationBuilder.DeleteData(
+                table: "Messages",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000003"));
+
+            migrationBuilder.DeleteData(
+                table: "Chats",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000001"));
+
+            migrationBuilder.DeleteData(
+                table: "Chats",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000002"));
+
+            migrationBuilder.DropColumn(
+                name: "NumberOfFriends",
+                table: "Users");
+
+            migrationBuilder.InsertData(
+                table: "Chats",
+                columns: new[] { "Id", "CreateDate", "CreatorId", "Image", "LastMessage", "Name", "NumberOfMessages", "NumberOfUsers", "Theme" },
+                values: new object[] { new Guid("00000000-0000-0000-0000-000000000001"), new DateTime(2019, 4, 5, 13, 28, 48, 567, DateTimeKind.Local).AddTicks(7823), new Guid("00000000-0000-0000-0000-000000000000"), null, null, "Awesome Chat", 0, 0, null });
+
+            migrationBuilder.InsertData(
+                table: "Chats",
+                columns: new[] { "Id", "CreateDate", "CreatorId", "Image", "LastMessage", "Name", "NumberOfMessages", "NumberOfUsers", "Theme" },
+                values: new object[] { new Guid("00000000-0000-0000-0000-000000000002"), new DateTime(2019, 4, 5, 11, 28, 48, 569, DateTimeKind.Local).AddTicks(6772), new Guid("00000000-0000-0000-0000-000000000000"), null, null, "Another awesome Chat", 0, 0, null });
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000001"),
+                column: "Created",
+                value: new DateTime(2019, 4, 5, 13, 28, 49, 467, DateTimeKind.Local).AddTicks(6780));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000002"),
+                column: "Created",
+                value: new DateTime(2019, 4, 5, 13, 28, 49, 467, DateTimeKind.Local).AddTicks(6914));
+
+            migrationBuilder.InsertData(
+                table: "Messages",
+                columns: new[] { "Id", "ChatId", "ImageName", "SendDate", "SenderId", "Text" },
+                values: new object[] { new Guid("00000000-0000-0000-0000-000000000003"), new Guid("00000000-0000-0000-0000-000000000001"), null, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new Guid("00000000-0000-0000-0000-000000000001"), "This is a chat with myself. Is this even possible?" });
+
+            migrationBuilder.InsertData(
+                table: "Messages",
+                columns: new[] { "Id", "ChatId", "ImageName", "SendDate", "SenderId", "Text" },
+                values: new object[] { new Guid("00000000-0000-0000-0000-000000000001"), new Guid("00000000-0000-0000-0000-000000000002"), null, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new Guid("00000000-0000-0000-0000-000000000001"), "Hello Jon. This is Tyrion" });
+
+            migrationBuilder.InsertData(
+                table: "Messages",
+                columns: new[] { "Id", "ChatId", "ImageName", "SendDate", "SenderId", "Text" },
+                values: new object[] { new Guid("00000000-0000-0000-0000-000000000002"), new Guid("00000000-0000-0000-0000-000000000002"), null, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new Guid("00000000-0000-0000-0000-000000000002"), "Hello Tyrion. I'm Jon" });
+
+            migrationBuilder.UpdateData(
+                table: "UserChats",
+                keyColumns: new[] { "ChatId", "UserId" },
+                keyValues: new object[] { new Guid("00000000-0000-0000-0000-000000000001"), new Guid("00000000-0000-0000-0000-000000000001") },
+                column: "Created",
+                value: new DateTime(2019, 4, 5, 13, 28, 49, 467, DateTimeKind.Local).AddTicks(9282));
+
+            migrationBuilder.UpdateData(
+                table: "UserChats",
+                keyColumns: new[] { "ChatId", "UserId" },
+                keyValues: new object[] { new Guid("00000000-0000-0000-0000-000000000002"), new Guid("00000000-0000-0000-0000-000000000001") },
+                column: "Created",
+                value: new DateTime(2019, 4, 5, 13, 28, 49, 467, DateTimeKind.Local).AddTicks(9398));
+
+            migrationBuilder.UpdateData(
+                table: "UserChats",
+                keyColumns: new[] { "ChatId", "UserId" },
+                keyValues: new object[] { new Guid("00000000-0000-0000-0000-000000000002"), new Guid("00000000-0000-0000-0000-000000000002") },
+                column: "Created",
+                value: new DateTime(2019, 4, 5, 13, 28, 49, 467, DateTimeKind.Local).AddTicks(9346));
+        }
+    }
+}
