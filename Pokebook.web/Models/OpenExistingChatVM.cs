9617a44cc89b5f4293c4df3572079a00d49d143f
jens philibert <jens.philibert@hotmail.be>
2019-04-10 16:03:50 +0200

    Added OpenExistingChat View and VM

---
+++ Pokebook.web/Models/OpenExistingChatVM.cs
@@ -0,0 +1,16 @@
+﻿using Pokebook.core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pokebook.web.Models
+{
+    public class OpenExistingChatVM
+    {
+        public Chat Chat { get; set; }
+        public string Username { get; set; }
+        public string Text { get; set; }
+        public User Me { get; set; }
+    }
+}
