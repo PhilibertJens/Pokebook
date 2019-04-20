1ae8325db8b6946c53affb4f138d40b7a83cfffa
jens philibert <jens.philibert@hotmail.be>
2019-04-20 18:45:02 +0200

    Added ProfileController

---
+++ Pokebook.web/Controllers/ProfileController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Pokebook.core.Models;
+using Pokebook.web.Helpers;
+using Pokebook.web.Models;
+
+namespace Pokebook.web.Controllers
+{
+    public class ProfileController : Controller
+    {
+        string baseuri = "https://localhost:44321/api";//poortnummer aanpassen!
+
+        public async Task<IActionResult> Index()
+        {
+            Guid userId = Guid.Parse(HttpContext.Session.GetString("UserId"));
+            string uri = $"{baseuri}/users/{userId}";
+            User user = WebApiHelper.GetApiResult<User>(uri);
+            ProfileIndexVM vm = new ProfileIndexVM()
+            {
+                me = user,
+                myProfile = new UserProfile
+                {
+                    ProfilePicture = "Pikachu.png",
+                    CoverPicture = "Mt._Molteau.png"
+                }
+            };
+            return View(vm);
+        }
+    }
+}
