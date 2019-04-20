96c1e819f26694228d3f5a2971941a558ed5b3c2
jens philibert <jens.philibert@hotmail.be>
2019-04-20 13:50:59 +0200

    Added v-on:click attribute and hidden input types to use in VueChat.js

--- Pokebook.web/wwwroot/js/chat.js
+++ Pokebook.web/wwwroot/js/chat.js
@@ -7,28 +7,30 @@ connection.on("ReceiveMessage", function (user, message) {
     var me = document.getElementById("user").value;
     var newMessage = "";
 
-    
+    //aanmaak nodige HTML elementen
     var li = document.createElement("li");
     var p = document.createElement("p");
     var spanTime = document.createElement("span");
     var spanLetter = document.createElement("span");
     var br = document.createElement("br");
 
+    //inhoud van HTML elementen
     p.textContent = msg;
     spanLetter.textContent = user.charAt(0);
     spanTime.textContent = GetTime();
 
+    //li opvullen met andere HTML elementen
     if (user !== me) li.appendChild(spanLetter);
     li.appendChild(p);
     if (user !== me) li.appendChild(br);
     li.appendChild(spanTime);
-    
+
+    //classes geven aan HTML elementen en toevoegen aan messagesList
     spanLetter.setAttribute("class", "eersteLetter");
     spanTime.setAttribute("class", "time");
 
     if (user === me) li.setAttribute("class", "bubble-me");
     else li.setAttribute("class", "bubble-other");
-    console.log(newMessage);
     document.getElementById("messagesList").appendChild(li);
 });
 
@@ -40,6 +42,7 @@ document.getElementById("sendButton").addEventListener("click", function (event)
     //var user = document.getElementById("userInput").value;
     var user = document.getElementById("user").value;
     var message = document.getElementById("messageInput").value;
+    document.getElementById("messageInput").value = "";
     connection.invoke("SendMessage", user, message).catch(function (err) {
         return console.error(err.toString());
     });
