25fd152d23543e159092445e239145186837fedd
jens philibert <jens.philibert@hotmail.be>
2019-04-20 15:49:36 +0200

    Added SetTimeout of 1 second to Join Chat

--- Pokebook.web/wwwroot/js/chat.js
+++ Pokebook.web/wwwroot/js/chat.js
@@ -1,39 +1,45 @@
 ﻿"use strict";
 var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+var chatId = document.getElementById("chatId").value;
+setTimeout(function () {//als er niet gewacht wordt dan wordt de JoinChat al gedaan voordat de chathub connection is gemaakt
+    //do something once
+    connection.invoke("JoinChat", chatId).catch(function (err) {
+        return console.error(err.toString());
+    });
+}, 1000);
 
-connection.on("ReceiveMessage", function (user, message, chatId) {
-    var myChatId = document.getElementById("chatId").value;
-    if (myChatId === chatId) {
-        var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g,
-            "&gt;");
-        var me = document.getElementById("user").value;
 
-        //aanmaak nodige HTML elementen
-        var li = document.createElement("li");
-        var p = document.createElement("p");
-        var spanTime = document.createElement("span");
-        var spanLetter = document.createElement("span");
-        var br = document.createElement("br");
+connection.on("ReceiveMessage", function (user, message) {
+    //enkel users die in dezelfde group (chat) zitten zullen dit ontvangen
+    var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g,
+        "&gt;");
+    var me = document.getElementById("user").value;
 
-        //inhoud van HTML elementen
-        p.textContent = msg;
-        spanLetter.textContent = user.charAt(0);
-        spanTime.textContent = GetTime();
+    //aanmaak nodige HTML elementen
+    var li = document.createElement("li");
+    var p = document.createElement("p");
+    var spanTime = document.createElement("span");
+    var spanLetter = document.createElement("span");
+    var br = document.createElement("br");
 
-        //li opvullen met andere HTML elementen
-        if (user !== me) li.appendChild(spanLetter);
-        li.appendChild(p);
-        if (user !== me) li.appendChild(br);
-        li.appendChild(spanTime);
+    //inhoud van HTML elementen
+    p.textContent = msg;
+    spanLetter.textContent = user.charAt(0);
+    spanTime.textContent = GetTime();
 
-        //classes geven aan HTML elementen en toevoegen aan messagesList
-        spanLetter.setAttribute("class", "eersteLetter");
-        spanTime.setAttribute("class", "time");
+    //li opvullen met andere HTML elementen
+    if (user !== me) li.appendChild(spanLetter);
+    li.appendChild(p);
+    if (user !== me) li.appendChild(br);
+    li.appendChild(spanTime);
 
-        if (user === me) li.setAttribute("class", "bubble-me");
-        else li.setAttribute("class", "bubble-other");
-        document.getElementById("messagesList").appendChild(li);
-    }
+    //classes geven aan HTML elementen en toevoegen aan messagesList
+    spanLetter.setAttribute("class", "eersteLetter");
+    spanTime.setAttribute("class", "time");
+
+    if (user === me) li.setAttribute("class", "bubble-me");
+    else li.setAttribute("class", "bubble-other");
+    document.getElementById("messagesList").appendChild(li);
 });
 
 connection.start().catch(function (err) {
@@ -41,7 +47,6 @@ connection.start().catch(function (err) {
 });
 
 document.getElementById("sendButton").addEventListener("click", function (event) {
-    //var user = document.getElementById("userInput").value;
     var user = document.getElementById("user").value;
     var message = document.getElementById("messageInput").value;
     var chatId = document.getElementById("chatId").value;
