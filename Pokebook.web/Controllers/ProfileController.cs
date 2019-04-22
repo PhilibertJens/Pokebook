3f67e64b9ffb65b17eca682548faad3b26b53a2a
jens philibert <jens.philibert@hotmail.be>
2019-04-22 14:49:51 +0200

    Added post Change method to ProfileController to upload image to api

--- Pokebook.web/Controllers/ProfileController.cs
+++ Pokebook.web/Controllers/ProfileController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -24,6 +26,51 @@ namespace Pokebook.web.Controllers
                 me = user
             };
             return View(vm);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Consumes("multipart/form-data")]
+        public async Task<IActionResult> Change(ProfileIndexVM userdata)
+        {
+            Guid userId = Guid.Parse(HttpContext.Session.GetString("UserId"));
+            string uri = $"{baseuri}/users/{userId}";
+            User user = WebApiHelper.GetApiResult<User>(uri);
+            string input = "";
+            if (ModelState.IsValid)
+            {
+                uri = $"{baseuri}/users/ProfilePicture";
+                using (HttpClient httpClient = new HttpClient())
+                {
+                    HttpContent fileStreamContent = new StreamContent(userdata.UploadedImage.OpenReadStream());
+                    fileStreamContent.Headers.ContentDisposition = new ContentDispositionHeaderValue("form-data")
+                    {
+                        Name = "file",
+                        FileName = userdata.UploadedImage.FileName
+                    };
+                    fileStreamContent.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+
+                    using (var formData = new MultipartFormDataContent())
+                    {
+                        formData.Add(fileStreamContent);
+                        HttpResponseMessage response = await httpClient.PostAsync(uri, formData);
+                        input = await response.Content.ReadAsStringAsync();
+                    }
+                }
+                if (input != "")
+                {
+                    user.ProfilePicture = userdata.UploadedImage.FileName;
+                    uri = $"{baseuri}/users/update";
+                    User updatedProfile = await WebApiHelper.PutCallAPI<User, User>(uri, user);
+                }
+                return new RedirectToActionResult("Index", "Profile", null);
+            }
+            
+            ProfileIndexVM vm = new ProfileIndexVM()
+            {
+                me = user
+            };
+            return View("Index", vm);
         }
     }
 }
--- Pokebook.web/Models/ProfileIndexVM.cs
+++ Pokebook.web/Models/ProfileIndexVM.cs
@@ -1,6 +1,8 @@
-﻿using Pokebook.core.Models;
+﻿using Microsoft.AspNetCore.Http;
+using Pokebook.core.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,5 +11,8 @@ namespace Pokebook.web.Models
     public class ProfileIndexVM
     {
         public User me { get; set; }
+
+        [Required(ErrorMessage = "No image found")]
+        public IFormFile UploadedImage { get; set; }
     }
 }
