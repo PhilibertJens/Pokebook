db1d59f863310e0bd00a661b8d40b0fa8b171412
jens philibert <jens.philibert@hotmail.be>
2019-04-27 11:11:59 +0200

    Added profile image preview with js

---
+++ Pokebook.web/wwwroot/js/preview.js
@@ -0,0 +1,17 @@
+﻿$(document).ready(function () {
+    console.log("De DOM is geladen!!");
+    $("#UploadedImage").on('change', showImagePreview);
+});
+
+function showImagePreview() {
+    var input = this;
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            $('#preview').attr('src', e.target.result);
+        };
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+
+
