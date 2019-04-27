1c059b1d237f576f660539bf179d0426013331d9
jens philibert <jens.philibert@hotmail.be>
2019-04-27 13:01:44 +0200

    Added tab navigation to switch between form

--- Pokebook.web/wwwroot/js/preview.js
+++ Pokebook.web/wwwroot/js/preview.js
@@ -1,17 +1,27 @@
 ﻿$(document).ready(function () {
     console.log("De DOM is geladen!!");
-    $("#UploadedImage").on('change', showImagePreview);
+    $("#UploadedProfileImage").on('change', showProfileImagePreview);
+    $("#UploadedCoverImage").on('change', showCoverImagePreview);
 });
 
-function showImagePreview() {
+function showProfileImagePreview() {
     var input = this;
     if (input.files && input.files[0]) {
         var reader = new FileReader();
         reader.onload = function (e) {
-            $('#preview').attr('src', e.target.result);
+            $('#preview-profile').attr('src', e.target.result);
         };
         reader.readAsDataURL(input.files[0]);
     }
 }
 
-
+function showCoverImagePreview() {
+    var input = this;
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            $('#preview-cover').attr('src', e.target.result);
+        };
+        reader.readAsDataURL(input.files[0]);
+    }
+}
