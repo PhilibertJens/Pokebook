9488ebeac824917bb8a61ad5af17e2c64ea2b9d1
jens philibert <jens.philibert@hotmail.be>
2019-05-01 17:13:44 +0200

    Added vueProfile.js class to navigate through profile information

---
+++ Pokebook.web/wwwroot/js/vueProfile.js
@@ -0,0 +1,33 @@
+﻿var app = new Vue(
+    {
+        el: '#profileInfo',
+        data: {
+            loadingMessage: 'Loading info...',
+            posts: null
+        },
+        methods: {
+            getUserInfo: function (e) {
+                this.processNavigation(e);
+                document.querySelector("#userInfo").removeAttribute("hidden");
+            },
+            getPokeInfo: function (e) {
+                this.processNavigation(e);
+                document.querySelector("#pokeInfo").removeAttribute("hidden");
+            },
+            getFriendsInfo: function (e) {
+                this.processNavigation(e);
+                document.querySelector("#friendInfo").removeAttribute("hidden");
+            },
+            processNavigation: function (e) {
+                items = document.querySelectorAll(".information div");
+                for (i = 0; i < items.length; i++) {
+                    items[i].setAttribute("hidden", true);
+                }
+                items = document.querySelectorAll(".links li");
+                for (i = 0; i < items.length; i++) {
+                    items[i].classList.remove("active");
+                }
+                e.target.classList.add("active");
+            }
+        }
+    });
