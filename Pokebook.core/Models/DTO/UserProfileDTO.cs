83f83ca3fe99df61560d99e3b26e8094d7bfa7db
jens philibert <jens.philibert@hotmail.be>
2019-05-04 10:51:13 +0200

    Added UserProfileDTO object to Pokebook.core

---
+++ Pokebook.core/Models/DTO/UserProfileDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pokebook.core.Models.DTO
+{
+    public class UserProfileDTO
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+    }
+}
