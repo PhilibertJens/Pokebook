40329ac024b04669ce606c026f01127a7a41105f
jens philibert <jens.philibert@hotmail.be>
2019-05-11 12:11:16 +0200

    Added addFriend method to vueFriendProfile.js

--- Pokebook.web/Models/ProfileFriendVM.cs
+++ Pokebook.web/Models/ProfileFriendVM.cs
@@ -9,25 +9,10 @@ namespace Pokebook.web.Models
 {
     public class ProfileFriendVM
     {
-        public User me { get; set; }
-        
-        public IFormFile UploadedProfileImage { get; set; }
-        public IFormFile UploadedCoverImage { get; set; }
+        public User Me { get; set; }
+        public User Friend { get; set; }
 
-        public string ProfilePicture { get; set; }
-        public string CoverPicture { get; set; }
-
-        public string FirstName { get; set; }
-
-        public string LastName { get; set; }
-
-        public string UserName { get; set; }
-
-        public string FavoritePokemon { get; set; }
-
-        public string FavoritePokemonGame { get; set; }
-
-        public ICollection<User> Friends { get; set; }
+        public ICollection<User> FriendsOfFriend { get; set; }
 
         public bool UserIsFriend { get; set; }
     }
