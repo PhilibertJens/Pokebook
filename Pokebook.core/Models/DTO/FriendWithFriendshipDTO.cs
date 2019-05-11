215505d22795cfcc89c4ae7257f6401c18d8a825
jens philibert <jens.philibert@hotmail.be>
2019-05-11 13:13:31 +0200

    Added FriendWithFriendshipsDTO class to Pokebook.core

---
+++ Pokebook.core/Models/DTO/FriendWithFriendshipDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pokebook.core.Models.DTO
+{
+    public class FriendWithFriendshipDTO
+    {
+        public User Friend { get; set; }
+        public Friendship Friendship { get; set; }
+    }
+}
