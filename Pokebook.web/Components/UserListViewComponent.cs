b45a25642f3cb976508ca86b722c73129bd18dd1
jens philibert <jens.philibert@hotmail.be>
2019-05-12 17:55:37 +0200

    Added UserListViewComponent with AllUsersVM

---
+++ Pokebook.web/Components/UserListViewComponent.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Pokebook.core.Models;
+using Pokebook.core.Models.DTO;
+using Pokebook.web.Helpers;
+using Pokebook.web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pokebook.web.Components
+{
+    public class UserListViewComponent : ViewComponent
+    {
+        public UserListViewComponent()
+        {
+            Constants constants = new Constants();
+            baseuri = $"https://localhost:{constants.Portnumber}/api";
+        }
+
+        string baseuri;
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            if (HttpContext.Session.GetString("UserId") != null)
+            {
+                Guid userId = Guid.Parse(HttpContext.Session.GetString("UserId"));
+                var uri = $"{baseuri}/users/simple/{userId}";
+                var userNameDTO = WebApiHelper.GetApiResult<UserSimpleDTO>(uri);
+                uri = $"{baseuri}/users/simple";
+                List<UserSimpleDTO> allUsers = WebApiHelper.GetApiResult<List<UserSimpleDTO>>(uri);
+                allUsers = allUsers.Where(a => a.UserName != userNameDTO.UserName).ToList();
+
+                AllUsersVM vm = new AllUsersVM
+                {
+                    AllUsers = allUsers
+                };
+
+                return View(vm);
+            }
+            return View(null);
+        }
+    }
+}
---
+++ Pokebook.web/Models/AllUsersVM.cs
@@ -0,0 +1,13 @@
+﻿using Pokebook.core.Models.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pokebook.web.Models
+{
+    public class AllUsersVM
+    {
+        public List<UserSimpleDTO> AllUsers { get; set; }
+    }
+}
