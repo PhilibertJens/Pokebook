d8b84efc72e1d676b33ea0e12c09eafa105e8d80
jens philibert <jens.philibert@hotmail.be>
2019-05-12 11:41:39 +0200

    Added fetchFriendRequests method to vueFriendRequests.js

--- Pokebook.web/wwwroot/js/vueFriendRequests.js
+++ Pokebook.web/wwwroot/js/vueFriendRequests.js
@@ -1,8 +1,29 @@
-﻿var app = new Vue(
+﻿var apiURL = 'https://localhost:44321/api/';
+
+var app = new Vue(
     {
         el: '.friendRequests',
         data: {
-            loadingMessage: 'Loading friendRequests...'
-            
+            loadingMessage: 'Loading friendRequests...',
+            friendRequests: null,
+        },
+        created: function() {
+            var self = this;
+            self.fetchFriendRequests();
+        },
+        methods: {
+            fetchFriendRequests: function () {
+                self = this;
+                var userId = document.getElementById("userId").value;
+                console.log(`${apiURL}friendships/GetFriendsToApprove/${userId}`);
+                fetch(`${apiURL}friendships/GetFriendsToApprove/${userId}`)
+                    .then(res => res.json())
+                    .then(function (requests) {
+                        self.friendRequests = requests;
+                        self.loadingMessage = 'Overview';
+                        console.log(requests);
+                    })
+                    .catch(err => console.error('Fout: ' + err));
+            }
         }
     });
