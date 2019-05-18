1ac95ad6d12f3543597ba68688085d37407d8ac9
jens philibert <jens.philibert@hotmail.be>
2019-05-18 13:59:17 +0200

    Added chat notification on nav bar

--- Pokebook.web/wwwroot/js/chatNotification.js
+++ Pokebook.web/wwwroot/js/chatNotification.js
@@ -1,14 +1,14 @@
 ﻿"use strict";
 var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
-var chatId = [];
-var chatIds = document.querySelectorAll('[data-id]');
-for (var i = 0; i < chatIds.length; i++) {
-    chatId.push(chatIds[i].getAttribute('data-id'));
+var chatIds = [];
+var chatIdSelector = document.querySelectorAll('[data-id]');
+for (var i = 0; i < chatIdSelector.length; i++) {
+    chatIds.push(chatIdSelector[i].getAttribute('data-id'));
 }
 
 setTimeout(function () {
-    for (var i = 0; i < chatId.length; i++) {
-        connection.invoke("JoinChat", chatId[i]).catch(function (err) {
+    for (var i = 0; i < chatIds.length; i++) {
+        connection.invoke("JoinChat", chatIds[i]).catch(function (err) {
             return console.error(err.toString());
     });
     }
@@ -16,12 +16,17 @@ setTimeout(function () {
 
 
 connection.on("ReceiveNotification", function (user, message, chatId) {
-    var chat = document.querySelectorAll('[data-id="' + chatId + '"]');
-    var span = chat[0].querySelector(".badge");//er is maar 1 span met class badge
-    var numberOfMessages = span.textContent;
-    if (numberOfMessages === "") numberOfMessages = 1;
-    else numberOfMessages = parseInt(numberOfMessages) +1;
-    span.innerHTML = numberOfMessages;
+    var myUsername = document.querySelectorAll('[data-username]')[0].getAttribute('data-username');
+    console.log(myUsername);
+    if (user !== myUsername) {
+        var chat = document.querySelectorAll('[data-id="' + chatId + '"]');
+        var span = chat[0].querySelector(".badge");//er is maar 1 span met class badge
+        var numberOfMessages = span.textContent;
+        if (numberOfMessages === "") numberOfMessages = 1;
+        else numberOfMessages = parseInt(numberOfMessages) +1;
+        span.innerHTML = numberOfMessages;
+        document.getElementsByClassName("layoutBadge")[0].style.display = "block";
+    }
 });
 
 connection.start().catch(function (err) {
