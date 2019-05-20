7dabd5a8b2e517454d969b34abc037c58120cc00
jens philibert <jens.philibert@hotmail.be>
2019-05-20 18:16:01 +0200

    Added extra methods to UserRepository to filter users

--- Pokebook.core/Repositories/Specific/UserRepository.cs
+++ Pokebook.core/Repositories/Specific/UserRepository.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using AutoMapper.QueryableExtensions;
+using Microsoft.EntityFrameworkCore;
 using Pokebook.core.Data;
 using Pokebook.core.Models;
 using Pokebook.core.Models.DTO;
@@ -38,10 +39,54 @@ namespace Pokebook.core.Repositories.Specific
             return PokebookContext.Users.Where(u => u.Id == Id)
                                   .ProjectTo<UserSimpleDTO>(mapper.ConfigurationProvider).FirstOrDefault();
         }
-
+        
         public List<UserSimpleDTO> GetUsersSimple()
         {
             return PokebookContext.Users.ProjectTo<UserSimpleDTO>(mapper.ConfigurationProvider).ToList();
+        }
+
+        public List<UserSimpleDTO> GetChatUsersSimple(Guid chatId)
+        {
+            var userList = new List<UserSimpleDTO>();
+            var allUserChats = PokebookContext.UserChats.Include(uc => uc.User).Where(uc => uc.ChatId == chatId).ToList();
+            foreach (var uc in allUserChats)
+            {
+                UserSimpleDTO userToAddDTO = GetUserSimple(uc.User.Id);
+                userList.Add(userToAddDTO);
+            }
+            return userList;
+        }
+
+        public List<User> GetChatUsers(Guid chatId)
+        {
+            var userList = new List<User>();
+            var allUserChats = PokebookContext.UserChats.Include(uc => uc.User).Where(uc => uc.ChatId == chatId).ToList();
+            foreach (var uc in allUserChats) userList.Add(uc.User);
+            return userList;
+        }
+
+        public List<User> GetRemainingUsers(Guid chatId)
+        {
+            var userList = GetChatUsers(chatId);
+            var allUsers = PokebookContext.Users.ToList();
+            foreach (var user in userList)
+            {
+                User toRemove = allUsers.Where(a => a.UserName == user.UserName).FirstOrDefault();
+                allUsers.Remove(toRemove);
+            }
+            return allUsers;
+        }
+
+        public List<UserSimpleDTO> GetRemainingUsersSimple(Guid chatId)
+        {
+            var allUsers = PokebookContext.Users.ProjectTo<UserSimpleDTO>(mapper.ConfigurationProvider).ToList();
+            var userList = GetChatUsers(chatId);
+            foreach (var user in userList)
+            {
+                UserSimpleDTO toRemove = allUsers.Where(a => a.UserName == user.UserName).FirstOrDefault();
+                allUsers.Remove(toRemove);
+            }
+            return allUsers;
         }
 
         public PokebookContext PokebookContext
