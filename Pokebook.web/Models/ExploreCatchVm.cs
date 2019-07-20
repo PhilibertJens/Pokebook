6997c2bc77a6a33a3da02f1d7fa6ac232d8de435
jens philibert <jens.philibert@hotmail.be>
2019-07-20 13:38:46 +0200

    Copied Explore VM's from other project

---
+++ Pokebook.web/Models/ExploreCatchVm.cs
@@ -0,0 +1,17 @@
+﻿using Pokebook.core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pokebook.web.Models
+{
+    public class ExploreCatchVm
+    {
+        public Pokemon AppearedPokemon { get; set; }
+        public int HP { get; set; }
+        public List<string> Moves { get; set; }
+        public string CheatingWarning { get; set; }
+        public string UserName { get; set; }
+    }
+}
