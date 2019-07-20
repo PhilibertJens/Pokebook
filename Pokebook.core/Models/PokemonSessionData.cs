c0f6b63f89f6a97d3d95ac7d9e446ed9ee55a109
jens philibert <jens.philibert@hotmail.be>
2019-07-20 13:39:32 +0200

    Copied Views from other project

---
+++ Pokebook.core/Models/PokemonSessionData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pokebook.core.Models
+{
+    public class PokemonSessionData
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public bool Caught { get; set; }/*blijft false als user vanuit GeneratePokemon of CatchProcesser url naar Gotcha veranderd*/
+        public string CheatingWarning { get; set; } /*wordt ingevuld bij Gotcha*/
+        public int HP { get; set; }
+        public List<string> Moves { get; set; }
+        //Er kunnen nog specifieke pokemon kenmerken worden toegevoegd
+    }
+}
