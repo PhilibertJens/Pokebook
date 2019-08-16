36f0b2abd270ee70169aeb3fc843d816123ccef3
jens philibert <jens.philibert@hotmail.be>
2019-08-16 15:01:53 +0200

    Added new api methods to PokemonCatchesController

--- Pokebook.api/Controllers/PokemonCatchesController.cs
+++ Pokebook.api/Controllers/PokemonCatchesController.cs
@@ -23,6 +23,29 @@ namespace Pokebook.api.Controllers
 
         private IHostingEnvironment _hostingEnvironment;
 
+        // GET: api/PokemonCatches
+        [HttpGet]
+        public override IActionResult Get(Guid id)
+        {
+            return Ok(unitOfWork.Pokemons.FindById(id));
+        }
 
+        // GET: api/PokemonCatches/name
+        [HttpGet]
+        [Route("CreateFromTemplate/{name}/{userId}")]
+        public async Task<IActionResult> CreateFromTemplate(string name, Guid userId)
+        {
+            Pokemon template = await unitOfWork.Pokemons.GetByName(name);
+            var createdPokemon = unitOfWork.PokemonCatches.CreateFromTemplate(template, userId);
+            return Ok(createdPokemon);
+        }
+
+        [HttpPost]
+        [Route("Add")]
+        public IActionResult AddPokemonCatch(PokemonCatch pokemon)
+        {
+            pokemon.Pokemon = null;//als dit niet gebeurt verschijnt er een duplicated insert error bij de Pokemons tabel
+            return Ok(Post(pokemon));
+        }
     }
 }
