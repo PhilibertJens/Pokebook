3075a47d1cb5e205b8942c932a310cebfad3adca
jens philibert <jens.philibert@hotmail.be>
2019-09-08 10:37:33 +0200

    Changed Constant classes

--- Pokebook.web/Constants.cs
+++ Pokebook.web/Constants.cs
@@ -7,8 +7,27 @@ namespace Pokebook.web
 {
     public class Constants
     {
-        public int Portnumber { get; } = 44321;//poortnummer aanpassen! Ook in js files niet vergeten!
-        public string apiLanAddress { get; } = "192.168.0.214:45456";//via deze adres info zijn de api calls beschikbaar voor de lan
-        //public string apiLanAddress { get; } = "localhost:44321";
+        public Constants()
+        {
+            /*Enkel deze 4 private fields moeten soms gewijzigd worden*/
+            portnumberServer = 44321;
+            portnumberClient = 45456;
+            hostnameServer = "localhost";
+            hostnameClient = "192.168.0.215";
+
+            ApiServerSideAddress = $"https://{hostnameServer}:{portnumberServer}";
+            ApiClientSideAddress = $"https://{hostnameClient}:{portnumberClient}";
+        }
+
+        private readonly string hostnameServer;
+        private readonly string hostnameClient;
+        private readonly int portnumberServer;
+        private readonly int portnumberClient;
+        public string ApiServerSideAddress { get; }
+        public string ApiClientSideAddress { get; }
+
+
+        //public string apiServerSideAddress { get; } = $"https://{hostname}:44321";
+        //public string apiClientSideAddress { get; } = "https://192.168.0.215:45456";
     }
 }
