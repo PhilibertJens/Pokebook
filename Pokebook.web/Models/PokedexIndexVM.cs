9340cb47c73fa77ce49b73c132a4f5bf97a7acfb
jens philibert <jens.philibert@hotmail.be>
2019-09-28 12:26:34 +0200

    Added necessary Views and Partial views

---
+++ Pokebook.web/Models/PokedexIndexVM.cs
@@ -0,0 +1,15 @@
+﻿using Pokebook.core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pokebook.web.Models
+{
+    public class PokedexIndexVM
+    {
+        public IEnumerable<Pokemon> AllPokemonWithTypeInfo { get; set; }
+        public string Username { get; set; }
+        public IEnumerable<PokemonCatch> AllCaughtPokemon { get; set; }
+    }
+}
