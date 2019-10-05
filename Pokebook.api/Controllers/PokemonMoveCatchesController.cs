44ec3ce8e67568b7de2808637122eda1ab55578f
jens philibert <jens.philibert@hotmail.be>
2019-10-05 10:54:20 +0200

    Added PokemonMoveCatchesController with repo

---
+++ Pokebook.api/Controllers/PokemonMoveCatchesController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Pokebook.core.Data;
+using Pokebook.core.Models;
+using Pokebook.core.Repositories.Specific;
+
+namespace Pokebook.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PokemonMoveCatchesController : ControllerCrudBase<PokemonMoveCatch>
+    {
+        public PokemonMoveCatchesController(PokebookContext dbc, IMapper m, PokemonMoveCatchRepository repo, IHostingEnvironment hostingEnvironment) : base(dbc, m, repo)
+        {
+            _hostingEnvironment = hostingEnvironment;
+        }
+
+        private IHostingEnvironment _hostingEnvironment;
+    }
+}
---
+++ Pokebook.core/Repositories/Specific/PokemonMoveCatchRepository.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Pokebook.core.Data;
+using Pokebook.core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokebook.core.Repositories.Specific
+{
+    public class PokemonMoveCatchRepository : MappingRepository<PokemonMoveCatch>, IPokemonMoveCatchRepository
+    {
+        public PokemonMoveCatchRepository(PokebookContext context, IMapper mapper) : base(context, mapper)
+        {
+
+        }
+
+        public PokebookContext PokebookContext
+        {
+            get { return db as PokebookContext; }
+        }
+    }
+}
