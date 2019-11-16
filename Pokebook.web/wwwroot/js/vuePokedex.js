422f56c85b2319f32dcb6aaa36fc983e257d300c
jens philibert <jens.philibert@hotmail.be>
2019-11-16 15:05:55 +0100

    Added vuePokedex.js to Pokedex Index.cshtml

---
+++ Pokebook.web/wwwroot/js/vuePokedex.js
@@ -0,0 +1,15 @@
+﻿var apiURL = `${server.apiClientSideAddress}/api/`;
+
+var app = new Vue(
+    {
+        el: '.poke-grid',
+            data: {
+                loadingMessage: 'Loading pokédex...'
+            },
+            created: function () {
+                var self = this;
+            },
+        methods: {
+
+        }
+    });
