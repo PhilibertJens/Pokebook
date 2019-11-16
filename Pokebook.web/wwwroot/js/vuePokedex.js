64af34c33ca038db5662246d94a3d0685d712130
jens philibert <jens.philibert@hotmail.be>
2019-11-16 18:16:05 +0100

    Added updatePokemonCatchesList to filter pokedex

--- Pokebook.web/wwwroot/js/vuePokedex.js
+++ Pokebook.web/wwwroot/js/vuePokedex.js
@@ -2,10 +2,11 @@
 
 var app = new Vue(
     {
-        el: '.poke-grid',
+        el: '#pokedex',
         data: {
             loadingMessage: 'Loading pokédex...',
             userId: '',
+            userValue: '',
             listAllPokemonCatches: [],
             listPokemonCatchesToEdit: []
         },
@@ -22,10 +23,26 @@ var app = new Vue(
                     .then(function (pokemonList) {
                         self.listAllPokemonCatches = pokemonList;
                         self.listPokemonCatchesToEdit = pokemonList;
-                        //console.log(pokemonList[0].pokemon.imgUrl);
+                        self.temp = pokemonList;
                         console.log(pokemonList);
                     })
                     .catch(err => console.error('Fout: ' + err));
+            },
+            updatePokemonCatchesList: function (e) {
+                /*als er items uit de list gewist worden gebeurt dit ook bij alle variabelen die naar deze list verwijzen vb. listAllPokemonCatches.
+                2 oplossingen:
+                - bij het weghalen van letters uit de input list wordt de volledige list terug opnieuw opgehaald en gefilterd --> veel dataverbruik maar eenvoudig
+                - de volledige list opslaan in local storage en ophalen wanneer er letters verwijderd worden --> minder dataverbruik maar moeilijker
+                eerst de eerste methode testen en nadien de tweede eens proberen.*/
+                self = this;
+                for (var i = 0; i < self.listPokemonCatchesToEdit.length; i++) {
+                    var name = self.listPokemonCatchesToEdit[i].pokemon.name.toLowerCase();
+                    name = name.substring(0, self.userValue.length);
+                    if (name !== self.userValue) {
+                        self.listPokemonCatchesToEdit.splice(i, 1);
+                        i--;
+                    }
+                }
             }
         }
     });
