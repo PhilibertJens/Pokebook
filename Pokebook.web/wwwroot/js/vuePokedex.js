f027a9512897223c951828053158c738aea71610
jens philibert <jens.philibert@hotmail.be>
2019-11-16 17:15:45 +0100

    Added getAllPokemonCatches method to vuePokedex

--- Pokebook.web/wwwroot/js/vuePokedex.js
+++ Pokebook.web/wwwroot/js/vuePokedex.js
@@ -3,13 +3,29 @@
 var app = new Vue(
     {
         el: '.poke-grid',
-            data: {
-                loadingMessage: 'Loading pokédex...'
-            },
-            created: function () {
-                var self = this;
-            },
+        data: {
+            loadingMessage: 'Loading pokédex...',
+            userId: '',
+            listAllPokemonCatches: [],
+            listPokemonCatchesToEdit: []
+        },
+        created: function () {
+            var self = this;
+            self.userId = document.querySelector('[data-userId]').getAttribute('data-userId');
+            self.getAllPokemonCatches();
+        },
         methods: {
-
+            getAllPokemonCatches: function (e) {
+                self = this;
+                fetch(`${apiURL}PokemonCatches/GetMyPokemon/${self.userId}`)
+                    .then(res => res.json())
+                    .then(function (pokemonList) {
+                        self.listAllPokemonCatches = pokemonList;
+                        self.listPokemonCatchesToEdit = pokemonList;
+                        //console.log(pokemonList[0].pokemon.imgUrl);
+                        console.log(pokemonList);
+                    })
+                    .catch(err => console.error('Fout: ' + err));
+            }
         }
     });
