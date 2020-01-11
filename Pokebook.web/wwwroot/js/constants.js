7553de4403c2acfa420dd392790adc375065d19c
jens philibert <jens.philibert@hotmail.be>
2020-01-11 14:35:24 +0100

    Added modal to change sorting type

--- Pokebook.web/wwwroot/js/constants.js
+++ Pokebook.web/wwwroot/js/constants.js
@@ -1,5 +1,5 @@
 ﻿var server = {
     //apiServerSideAddress: "https://192.168.0.215:45456",
     apiServerSideAddress: "https://localhost:44321",
-    apiClientSideAddress: "https://192.168.0.215:45456"
+    apiClientSideAddress: "https://192.168.0.213:45456"
 };
